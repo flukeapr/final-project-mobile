1632c14d97909881607bac25a3faa2c6ebcf699d
flukeapr <wngsphlc@gmail.com>
2024-12-27 11:58:49 +0700

    change URL config

--- global.js
+++ global.js
@@ -1,2 +1,5 @@
 global.session = null
-global.URL = "http://10.0.2.2:3000"
+global.URL = "http://digiproj.sut.ac.th:3002";
+// ? http://digiproj.sut.ac.th:3002
+
+// ? https://a300-2405-9800-bc20-18e5-885-5ac-2c39-938f.ngrok-free.app use only for yesterday
